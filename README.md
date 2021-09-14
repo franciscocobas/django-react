ee993e084a726a8354f3c278d26f15301a8c84e9
Francisco Cobas <fcarocena@gmail.com>
2021-09-14 08:50:38 -0300

    Add run server to README.md and starting modifying frontend

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Django & React App
+
+Run server
+`cd musci_controller`
+
+`python3 manage.py runserver`
--- music_controller/frontend/src/components/CreateRoomPage.tsx
+++ music_controller/frontend/src/components/CreateRoomPage.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const deafultVotes = 2;
 
 const CreateRoomPage = () => {
-  return <div>Hola</div>;
+  return (
+    <div>
+      <div>
+        <h4>Create a Room</h4>
+      </div>
+    </div>
+  );
 };
 
 export default CreateRoomPage;
